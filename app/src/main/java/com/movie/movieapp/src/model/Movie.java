5617f17207ed94c9926ca48a1acea139e9492b0f
Radu Falcusan <falcusanradudan@gmail.com>
2018-04-21 10:40:42 +0300

    first commit

---
+++ app/src/main/java/com/movie/movieapp/src/model/Movie.java
@@ -0,0 +1,94 @@
+package com.movie.movieapp.src.model;
+
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
+public class Movie {
+
+    @PrimaryKey(autoGenerate = true)
+    private Integer idMovie;
+
+    @ColumnInfo(name = "name")
+    private String name;
+
+    @ColumnInfo(name = "description")
+    private String description;
+
+    @ColumnInfo(name = "genre")
+    private String genre;
+
+    @ColumnInfo(name = "note")
+    private Integer note;
+
+    @ColumnInfo(name = "status")
+    private String status;
+
+
+    public Movie(Integer idMovie, String name, String description, String genre, Integer note, String status) {
+        this.idMovie = idMovie;
+        this.name = name;
+        this.description = description;
+        this.genre = genre;
+        this.note = note;
+        this.status = status;
+    }
+
+    public Integer getIdMovie() {
+        return idMovie;
+    }
+
+    public void setIdMovie(Integer idMovie) {
+        this.idMovie = idMovie;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Integer getNote() {
+        return note;
+    }
+
+    public void setNote(Integer note) {
+        this.note = note;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/movie/movieapp/src/model/User.java
@@ -0,0 +1,61 @@
+package com.movie.movieapp.src.model;
+
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity
+public class User {
+
+    @PrimaryKey(autoGenerate = true)
+    private Integer idUser;
+
+    @ColumnInfo(name = "username")
+    private String username;
+
+    @ColumnInfo(name = "password")
+    private String password;
+
+    @ColumnInfo(name = "type")
+    private String type;
+
+    public User(Integer idUser, String username, String password, String type) {
+        this.idUser = idUser;
+        this.username = username;
+        this.password = password;
+        this.type = type;
+    }
+
+    public Integer getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(Integer idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
